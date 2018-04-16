b42a36ac9f401bad7c5347d14bc51edba866e8b8
oppofind <836575280@qq.com>
2018-04-16 09:01:13 +0800

    优化shell脚本

--- install-hadoop-standalone/install.sh
+++ install-hadoop-standalone/install.sh
@@ -9,14 +9,14 @@ HADOOP_NAME_TAR=$HADOOP_NAME.tar.gz
 
 HADOOP_HOME=/usr/local/hadoop
 
-# passwordless ssh
+# =======================passwordless ssh===========================
 ssh-keygen -q -N "" -t dsa -f /etc/ssh/ssh_host_dsa_key
 ssh-keygen -q -N "" -t rsa -f /etc/ssh/ssh_host_rsa_key
 ssh-keygen -q -N "" -t rsa -f /root/.ssh/id_rsa
 cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 
 cd /usr/local
-# download hadoop
+# ========================download hadoop===========================
 if [ ! -f "$HADOOP_NAME_TAR" ]
 then
   wget http://mirrors.hust.edu.cn/apache/hadoop/common/$HADOOP_NAME/$HADOOP_NAME.tar.gz
@@ -29,30 +29,32 @@ tar -zxvf $HADOOP_NAME_TAR
 # rename hadoop
 mv $HADOOP_NAME hadoop
 
-# create customer data dir
+# ========================create customer data dir==================
 echo "create customer data dir"
 mkdir -p $HADOOP_HOME/custom/tmp
 mkdir -p $HADOOP_HOME/custom/hdfs
 mkdir -p $HADOOP_HOME/custom/hdfs/data
 mkdir -p  $HADOOP_HOME/custom/hdfs/name
 
-# set hadoop env
+# ========================set hadoop env============================
 echo "# set hadoop environment" >> /etc/profile
 echo "export HADOOP_HOME=/usr/local/hadoop" >> /etc/profile
 echo "export PATH=\$PATH:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin" >> /etc/profile
 
 source /etc/profile
-
+# ========================replace config============================
 echo "Current work home is $CUR_PATH"
 # copy xml config
 cp $CUR_PATH/conf/*.xml $HADOOP_HOME/etc/hadoop
 # copy
 cp $CUR_PATH/bash/*.sh $HADOOP_HOME/etc/hadoop
 
+# ========================Format Hadoop Namenode===================
 echo "Format Hadoop Namenode"
 
 hdfs namenode -format
 
+# =========================Finish install==========================
 echo "Finish hadoop standalone install"
 
 echo "You could start hadoop server in $HADOOP_HOME/sbin"
--- install-jdk/install-jdk.sh
+++ install-jdk/install-jdk.sh
@@ -13,23 +13,23 @@ fi
 
 cd $JDK_DIR
 
-# download jdk
+# ===================download oracle jdk=============================
 if [ ! -f "$JDK_NAME" ]
 then
   wget --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie;" http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-linux-x64.tar.gz 
 fi
 
-
+# ===================install jdk=====================================
 #extract jdk
 tar -zxvf $JDK_NAME
 
 # rename jdk
 mv jdk1.* jdk8
 
-#set java environment
+# ===================SET JAVA ENV=====================================
 if ! grep "JAVA_HOME=/usr/local/java/jdk1.8.0_131" /etc/profile
 then
-    echo "#set java environment" >> /etc/profile
+    echo "# set java environment" >> /etc/profile
     echo "export JAVA_HOME=/usr/local/java/jdk8" >> /etc/profile
     echo "export JRE_HOME=/usr/local/java/jdk8/jre/" >> /etc/profile
     echo "export CLASSPATH=.:\$CLASSPATH:\$JAVA_HOME/lib:\$JRE_HOME/lib" >> /etc/profile
