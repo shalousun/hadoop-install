44c905819712ad408bbf811a3e21ff1b33b42a3e
oppofind <836575280@qq.com>
2018-04-20 01:27:19 +0800

    修改hadoop集群安装

--- install-hadoop-cluster/README.md
+++ install-hadoop-cluster/README.md
@@ -1,7 +1,16 @@
 
+hadoop集群安装，手动安装的详细过程参考本模块doc中的文档，为了快速搭建一个master、两个slave的小规模完全分布式环境，
+目前只需要将规划三台机器修改为master、slave1、slave2，然后根据ip地址修改hosts，修改完后再master主机上执行install.sh
+安装脚本就能完成hadoop的安装，完成安装后使用scp将hadoop复制到slave1和slave2主机上即可。如果自己主机名的命名和
+本模块中的设置不一致也不想修改原主机名，则安装doc目录下的手动安装文档修改conf和bash中的对应文件后再执行安装脚本。
+
+# 环境要求
+1. jdk(参考install-jdk)
+2. 设置免密登录(参考install-passless)
 
 # 修改hosts
 
+例如：
 ```
 echo '
 192.168.248.145   master
--- install-hadoop-cluster/copy.sh
+++ install-hadoop-cluster/copy.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 SERVERS="master slave1 slave2"
-PASSWORD=123456
-
-
-sh_copy_id_to_all() {
-    for SERVER in $SERVERS
-    do
-        auto_ssh_copy_id $SERVER $PASSWORD
-    done
-}
-
-ssh_copy_id_to_all
 
 sh_copy_profile(){
     scp /etc/profile slave1:/etc/
@@ -24,3 +13,5 @@ sh_copy_hadoop(){
     scp -r /usr/local/hadoop slave1:/usr/local
     scp -r /usr/local/hadoop slave2:/usr/local
 }
+
+sh_copy_hadoop
