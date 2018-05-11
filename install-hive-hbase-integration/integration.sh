ec2b2c337b718a2f295d4cdf303ba9d316c16a14
oppofind <836575280@qq.com>
2018-05-11 11:21:11 +0800

    修改hbase和hive集成时的脚本提示输出

--- install-hive-hbase-integration/integration.sh
+++ install-hive-hbase-integration/integration.sh
@@ -6,7 +6,7 @@ CUR_PATH=$(cd `dirname $0`;pwd)
 HIVE_HOME=/usr/local/hive
 
 # ============REPLACE CONFIG==========================
-echo "Current work home is $CUR_PATH"
+echo "INFO: Current work home is $CUR_PATH"
 # copy xml config
 cp $CUR_PATH/conf/*.xml $HIVE_HOME/conf
 # copy
@@ -15,5 +15,5 @@ cp $CUR_PATH/bash/*.sh $HIVE_HOME/conf
 # =============COPY JARS TO LIB=======================
 cp $CUR_PATH/lib/*.jar $HIVE_HOME/lib
 
-echo "Finish Hive and HBase"
+echo "INFO: Finish Hive and HBase"
 
